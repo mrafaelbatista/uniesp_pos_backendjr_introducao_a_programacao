a92c28eb0b7e9c304961326870b91511da526eb9
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2022-09-24 13:41:21 -0300

    Slide 02 - Estrutura Condicional

--- aula_01/estrutura_sequencial/exemplos/slide_02.py
+++ aula_01/estrutura_sequencial/exemplos/slide_02.py
@@ -7,17 +7,33 @@
 # media_anual = (n1 + n2 + n3 + n4) / 4
 # print(f"A média anual do aluno foi: {media_anual:.2f}")
 
-print("---- Sitação Problema 2 -----")
+# print("---- Sitação Problema 2 -----")
+#
+# raio = float(input("Digite o Raio: "))
+# h = float(input("Digite a altura: "))
+# PI = 3.14
+#
+# area_cilindro = (PI * (raio ** 2)) + (2 * PI * raio * h)
+# qtd_litros = area_cilindro / 3
+# qtd_latas = qtd_litros / 5
+# custo_total = qtd_latas * 50.0
+#
+# print("Para pintar o cilindro precisamos de:")
+# print(f"{qtd_latas:0.2f} latas de tinta.")
+# print(f"A um custo de R$ {custo_total:0.2f}")
 
-raio = float(input("Digite o Raio: "))
-h = float(input("Digite a altura: "))
-PI = 3.14
+print("---- Sitação Problema 3 -----")
 
-area_cilindro = (PI * (raio ** 2)) + (2 * PI * raio * h)
-qtd_litros = area_cilindro / 3
-qtd_latas = qtd_litros / 5
-custo_total = qtd_latas * 50.0
+idade = int(input("Digite a sua idade: "))
+ingresso = input("Ingresso VIP ou PISTA?")
 
-print("Para pintar o cilindro precisamos de:")
-print(f"{qtd_latas:0.2f} latas de tinta.")
-print(f"A um custo de R$ {custo_total:0.2f}")
+if idade >= 18:
+    if ingresso.upper().strip() == "VIP":
+        print("Siga para o primeiro andar")
+    elif ingresso.upper().strip() == "PISTA":
+        print("Siga pelo corredor!")
+    else:
+        print("Volte para a fila e compre um ingresso!")
+else:
+    print("É melhor voltar pra casa!")
+    print("Vou chamar o conselho tutelar!!! :(")

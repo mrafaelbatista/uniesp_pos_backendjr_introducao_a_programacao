d081d9a14156c6111d55b546d3f2a3aebb2b4eea
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2022-10-22 14:00:03 -0300

    Aula 22/10 - 13:59

---
+++ aula_03/atividades_sala/atividade_03.py
@@ -0,0 +1,66 @@
+import os
+
+def contador_palavras(filename):
+    with open(filename, 'r', encoding="utf-8") as file_object:
+        conteudo = file_object.read()
+    palavras = conteudo.split()
+    return len(palavras)
+
+
+def guardar_info(titulo, quant_palavras, quant_bytes):
+
+    info = f"" \
+           f"Titulo: {titulo};" \
+           f"Qtd Palavras: {quant_palavras};" \
+           f"Qtd Bytes: {quant_bytes}\n"
+
+    with open("database.txt", "a", encoding="utf-8") as file_object:
+        file_object.write(info)
+
+
+def ler_database():
+
+    with open("database.txt", "r", encoding="utf-8") as file_object:
+        for linha in file_object:
+            print(linha.rstrip())
+
+
+while True:
+
+    opcao = int(input(
+        "1 - The Prince\n"
+        "2 - A Book Of German Lyric\n"
+        "3 - The eBook is 40\n"
+        "9 - Ler Base de Dados\n"
+        "0 - Sair "
+    ))
+
+    if opcao == 1:
+        print(f"O livro tem:\n{contador_palavras('the_prince.txt')}")
+        guardar_info(
+            "The Prince",
+            contador_palavras('the_prince.txt'),
+            os.path.getsize('the_prince.txt')
+        )
+
+    elif opcao == 2:
+        print(contador_palavras("a_book_of_german_lyric.txt"))
+        guardar_info(
+            "A book of German Lyric",
+            contador_palavras('a_book_of_german_lyric.txt'),
+            os.path.getsize('a_book_of_german_lyric.txt')
+        )
+
+    elif opcao == 3:
+        print(contador_palavras("the_ebook_is_40.txt"))
+        guardar_info(
+            "The Ebook is 40",
+            contador_palavras('the_ebook_is_40.txt'),
+            os.path.getsize('the_ebook_is_40.txt')
+        )
+
+    elif opcao == 9:
+        ler_database()
+
+    elif opcao == 0:
+        break

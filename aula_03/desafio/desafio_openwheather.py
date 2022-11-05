6a0a609dcf45387e9dd9cbc5747bbde19374a5df
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2022-11-04 21:24:42 -0300

    Desafio OpenWheatherMap

--- aula_03/desafio/desafio_openwheather.py
+++ aula_03/desafio/desafio_openwheather.py
@@ -1,20 +1,14 @@
-messias.batista@iesp.edu.br
-Whatsapp: 83 9 9609 8112
-
-
 import requests
 import json
 
-API_KEY = '2f2f555888d23e5a36c5aad583ebfae9'
+API_KEY = ''
 LAT = -5.79448
 LOG = -35.211
 url = (f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LOG}&appid={API_KEY}")
 
-resposta   = requests.request("GET", url)
-objetos    = json.loads(resposta.text)
-# dados      = objetos['dados']
+resposta = requests.request("GET", url)
+objetos = json.loads(resposta.text)
+# dados = objetos['dados']
 
 for i in objetos:
     print(f"{i} :: {objetos[i]}")
-
-# https://api.openweathermap.org/data/2.5/weather?lat={-7.11532}&lon={-34.861}&appid={2f2f555888d23e5a36c5aad583ebfae9}

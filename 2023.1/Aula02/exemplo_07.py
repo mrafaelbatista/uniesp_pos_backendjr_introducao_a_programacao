7a758c1540fdbe933bb5a21d1062f0212716f96d
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2023-06-04 10:32:35 -0300

    Update 20230604

--- 2023.1/Aula02/exemplo_07.py
+++ 2023.1/Aula02/exemplo_07.py
@@ -12,4 +12,28 @@ print(frutas)
 frutas[1] = 'abacate'
 print(frutas)
 frutas.insert(2, 'morango')
+frutas.insert(4, 'graviola')
+frutas.insert(1, 'pitanga')
+print(frutas)
+
+del frutas[1]
+print(frutas)
+del frutas[4]
+print(frutas)
+frutas.insert(5, 'melancia')
+print(frutas)
+indice_melancia = frutas.index('melancia')
+print(f'O índice da melancia é: {indice_melancia}')
+del frutas[indice_melancia]
+print(frutas)
+frutas.remove('kiwi')
+print(frutas)
+print(f'O tamanho da lista é: {len(frutas)}')
+frutas.insert(2, 'abacaxi')
+print(frutas)
+indice_abacaxi = frutas.index('abacaxi')
+abacaxi_pop = frutas.pop(indice_abacaxi)
+print(frutas)
+print(f'A fruta deletada foi: {abacaxi_pop}')
+frutas.append('Pitaya')
 print(frutas)

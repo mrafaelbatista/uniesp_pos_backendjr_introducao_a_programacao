4ce74c099353e0277dec6f67b6dced9efc228639
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2023-06-17 15:45:50 -0300

    aula

---
+++ 2023.1/Aula03/09_exemplos_funcoes.py
@@ -0,0 +1,23 @@
+import minhas_funcoes
+from random import randint
+
+while True:
+    op = input('Digite a opção: \n'
+               '1 - Somar\n'
+               '2 - Subtrair\n'
+               '3 - Sair')
+    op = int(op)
+
+    match op:
+        case 1:
+            resultado = minhas_funcoes.somar(
+                randint(1, 5), randint(1, 5))
+            print(f'A soma foi: {resultado}')
+        case 2:
+            resultado = minhas_funcoes.subtrair(
+                randint(100, 200), randint(100, 200))
+            print(f'A subtração foi: {resultado}')
+        case 3:
+            break
+        case _:
+            print(f'Opção inválida!')
---
+++ 2023.1/Aula03/minhas_funcoes.py
@@ -0,0 +1,13 @@
+def somar(*args):
+    resultado = 0
+    for i in args:
+        resultado += i
+
+    return resultado
+
+def subtrair(*args):
+    resultado = 0
+    for i in args:
+        resultado -= i
+
+    return resultado

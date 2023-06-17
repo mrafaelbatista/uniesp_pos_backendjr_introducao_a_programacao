187f8e83e78d04fc2639da2f67189a055a383e01
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2023-06-17 10:58:50 -0300

    Aula

--- 2023.1/Aula03/02_exemplo_dict.py
+++ 2023.1/Aula03/02_exemplo_dict.py
@@ -1,3 +1,4 @@
+# Criando dicionários
 professor = {
     'nome': 'Messias',
     'idade': 36
@@ -6,9 +7,11 @@ professor = {
 aluno = {'nome': 'Aluno 1',
          'idade': 123}
 
+# Imprimindo valores de um dicionários
 print(professor['nome'])
 print(aluno['nome'])
 
+# Criando um dicionário com vários níveis de dados
 professores = {
     'Messias': {
         'idade': 36,
@@ -17,3 +20,35 @@ professores = {
         'idade': 53,
         'disciplinas': ['BD I', 'BD II']}
 }
+
+# Acessando valores no meu dicionário
+print(f'O professor Messias'
+      f' tem {professores["Messias"]["idade"]}'
+      f' anos, e ministrar as disciplinas'
+      f' de {professores["Messias"]["disciplinas"]}')
+
+# Adicionando novos dados
+professores['Messias']['email'] = 'mrafaelbatista@gmail.com'
+professores['Messias']['cpf'] = '000.000.000-00'
+# Dicionário com novos dados
+print(professores['Messias'])
+
+# <--- Lembrando! --->
+# Tipos primitivos: int, float, string, boolean
+# Tipos novos: lista, tupla, dicionário
+
+# Identificando o tipo de um dicionários
+print(type(professores))
+
+# Acessando as chaves e items
+print(professores.keys())
+print(professores.items())
+
+# Tipos das chaves e items
+print(type(professores.keys()))
+print(type(professores.items()))
+
+# Exemplo de delete de uma chave
+print(professores)
+del professores['Messias']['cpf']
+print(professores)

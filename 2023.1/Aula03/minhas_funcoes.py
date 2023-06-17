abfd11f25cb3a5648a3aa7488c4a3c813d6f0684
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2023-06-17 15:55:12 -0300

    Aula

--- 2023.1/Aula03/minhas_funcoes.py
+++ 2023.1/Aula03/minhas_funcoes.py
@@ -2,12 +2,10 @@ def somar(*args):
     resultado = 0
     for i in args:
         resultado += i
-
     return resultado
 
 def subtrair(*args):
     resultado = 0
     for i in args:
         resultado -= i
-
     return resultado

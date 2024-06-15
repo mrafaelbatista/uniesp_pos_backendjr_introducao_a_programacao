55185ba30ab2e485def3ff3a8486db450424251c
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2024-06-15 10:34:19 -0300

    Turma 24.1

---
+++ aula02/exemplos/sp_01.py
@@ -0,0 +1,10 @@
+# Situação Problema 01 - Cálculo da média Bimestral
+
+# Forma diferente de criar diversas variáveis
+n1, n2, n3, n4 = 10, 8, 5, 9
+
+# Cálculo da média
+media = (n1 + n2 + n3 + n4) / 4
+
+# Exibição da média
+print(f"Média: {media:.2f}")
---
+++ aula02/exemplos/sp_02.py
@@ -0,0 +1,22 @@
+raio = float(input("Digite o raio do cilindro: "))
+altura = float(input("Digite a altura do cilindro: "))
+
+PI = 3.14
+
+# Área do cilindro
+area_lateral = 2 * PI * raio * altura
+area_base = PI * (raio ** 2)
+area_cilindro = area_lateral + area_base
+
+# Cálculo da pintura
+quantidade_litros = area_cilindro / 3
+# A função round() arredonda o valor para o inteiro
+# mais próximo
+quantidade_latas = round(quantidade_litros / 5)
+
+# Custo total
+custo_total = quantidade_latas * 50
+
+print(f"Quantidade de latas: {quantidade_latas}")
+print(f"Custo total: R$ {custo_total:.2f}")
+

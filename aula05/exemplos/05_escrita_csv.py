f2bc1a2ecbb2157c890729889aa40e478ba99749
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2024-07-20 11:58:41 -0300

    Turma 24.1

---
+++ aula05/exemplos/05_escrita_csv.py
@@ -0,0 +1,18 @@
+import csv
+
+filename = 'escrevendo.csv'
+
+# lista = [['Messias', 38],
+#          ['Luiza', 5],
+#          ['João Pedro', 6],
+#          ['Tio Pedro', 41]]
+
+lista = ['Messias', 38]
+
+with open(filename, 'w', newline='', encoding='utf-8') as arquivo:
+    writer = csv.writer(arquivo)
+
+    # writer.writerows -> Armazena uma Matriz
+    # writer.writerow -> Armazena uma Lista
+
+    writer.writerow(lista)
